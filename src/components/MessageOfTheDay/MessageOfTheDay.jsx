6244aa36d759eea9bb8714b5ceee1bd66a1645f4
Rikki Soriano <rikkilanz@gmail.com>
2023-02-23 21:11:41 -0800

    Update MessageOfTheDay.jsx

--- src/components/MessageOfTheDay/MessageOfTheDay.jsx
+++ src/components/MessageOfTheDay/MessageOfTheDay.jsx
@@ -4,7 +4,7 @@ import './MessageOfTheDay.css';
 function MessageOfTheDay() {
     return(
         <div className="motd">
-            <div className="message"><h5 className="message">This is NOWWeather!</h5></div>
+            <div className="message"><h5 className="message">This is Today Weather!</h5></div>
             <p className="copyright">Built with React.js | &copy; Rikki Soriano</p>
         </div>
     )
